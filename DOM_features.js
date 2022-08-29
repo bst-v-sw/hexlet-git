98835493fc6932578ddfb20469a56c62fbbc6751
Vadim <110719459+bst-v-sw@users.noreply.github.com>
2022-08-29 08:23:54 +0300

    fix dom_features.js

--- DOM_features.js
+++ DOM_features.js
@@ -4,12 +4,20 @@ document.body.myData = {
     title: 'Imperator'
 };
 console.log(document.body.myData.title); // Imperator
-
+//==============================================================
 // Есть также методы
 document.body.sayTagName = function() {
     console.log(this.tagName);
 };
 
 document.body.sayTagName(); // BODY (значением "this" в этом методе будет document.body)
+//====================================================
+// Также можно изменять встроенные прототипы, такие как Element.prototype и добавлять новые методы ко всем элементам:
+Element.prototype.sayHi = function() {
+    alert(`Hello, I'm ${this.tagName}`);
+};
+
+document.documentElement.sayHi(); // Hello, I'm HTML
+document.body.sayHi(); // Hello, I'm BODY
 
 
